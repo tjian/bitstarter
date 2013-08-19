79214fae14125edab9f13c5dc2c29bf088c65482
Thanabordi Jianrungsang <sunsk128@gmail.com>
2013-08-19 02:51:40 +0000

    context now comes from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 app.use(express.logger());
 
+var context_file = "index.html";
+var context_buf = fs.readFileSync(context_file);
+var context = context_buf.toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(context);
 });
 
 var port = process.env.PORT || 5000;
